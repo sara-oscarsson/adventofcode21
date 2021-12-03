1c733550b558c9c1de68dd29f5f6518f7e3f6911
Sara Oscarsson <oscarsson.sara@hotmail.com>
2021-12-03 08:30:28 +0100

    day-one-solution

---
+++ day1.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+//Read file with all the input and turn it into a string
+const inputNumbers = fs.readFileSync('./input.txt').toString();
+//Split the string and remove all spaces, return an array with numbers
+const myArray = inputNumbers.split('\n').map(Number);
+
+
+let previousInput = myArray[0];
+let counter = 0;
+
+//Loop through array and keep count of all increases 
+for (let i = 0; i < myArray.length; i++) {
+    if(myArray[i] > previousInput) {
+        counter++;
+    }
+    //Keep track of the previous number to be able to compare with the next number
+    previousInput = myArray[i];
+    
+}
+
+console.log(`The answer to day one is: ${ counter }`);
+
